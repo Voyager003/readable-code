6d50b61a71f73ff7525bc64389f012182d7407fa
voyager003 <miraclex000@gmail.com>
2024-10-16 19:53:40 +0900

    test: 이용권 종합 정보 테스트

--- src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StudyCafeSeatPassTest {
 
@@ -38,6 +37,24 @@ class StudyCafeSeatPassTest {
         assertThat(cannotUseLocker).isFalse();
     }
 
+    @Test
+    @DisplayName("이용권을 생성 후, 이용권 종합 정보 테스트")
+    void passTotalTest() {
+        /*
+         * given : 주간(WEEKLY) 이용권을 생성
+         * when : 이용권 타입, 대여 기간, 가격, 할인 가격이 주어졌을 때
+         * then : 주어진 조건에 맞는 객체가 생성되었는지 검증
+         */
+        StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(StudyCafePassType.WEEKLY, 12, 400000, 0.15);
 
+        StudyCafePassType passType = seatPass.getPassType();
+        int duration = seatPass.getDuration();
+        int price = seatPass.getPrice();
+        int discountPrice = seatPass.getDiscountPrice();
 
+        assertThat(passType).isEqualTo(StudyCafePassType.WEEKLY);
+        assertThat(duration).isEqualTo(12);
+        assertThat(price).isEqualTo(400_000);
+        assertThat(discountPrice).isEqualTo(60_000);
+    }
 }
